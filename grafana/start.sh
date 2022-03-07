869cfa8f6560255b34069f981a711a754a3e3463
Pops Pops <contact@pops.one>
2022-03-07 16:03:36 +0000

    [grafana] update start.sh - making sure data folder is not root owned

--- grafana/start.sh
+++ grafana/start.sh
@@ -19,3 +19,5 @@ GF_USERS_ALLOW_SIGN_UP=false \
 PROMETHEUS_CONFIG="./data/prometheus.yml" \
 GRAFANA_CONFIG="./data/grafana.ini" \
 docker-compose up -d --remove-orphans --build "$@"
+
+sudo chown -R $USER:$USER data
